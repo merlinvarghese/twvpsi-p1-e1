75c7ba789da115e64605aec437be7331c56290b6
Suchi <tw18@TW18.local>
2019-08-28 12:49:04 +0530

    [Suchitra,Shwetambari] Add. area of rectangle

--- src/main/com/tw/vapasi/Rectangle.java
+++ src/main/com/tw/vapasi/Rectangle.java
@@ -1,4 +1,16 @@
+/* Defines a closed geometrical shape with four sides at an angle of 90 degree each */
 package com.tw.vapasi;
 
-public class Rectangle {
+class Rectangle {
+    private float length;
+    private float width;
+
+    Rectangle(float rectLength, float rectWidth) {
+        length = rectLength;
+        width = rectWidth;
+    }
+
+    float area() {
+        return length * width;
+    }
 }
--- src/test/com/tw/vapasi/RectangleTest.java
+++ src/test/com/tw/vapasi/RectangleTest.java
@@ -1,4 +1,17 @@
 package com.tw.vapasi;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RectangleTest {
+class RectangleTest {
+    @Test
+    void verifyAreaIs6ForInput2And3() {
+        Rectangle rectangle = new Rectangle(2, 3);
+        assertEquals(6, rectangle.area());
+    }
+
+    @Test
+    void verifyAreaIs8ForInput2And4() {
+        Rectangle rectangle = new Rectangle(2, 4);
+        assertEquals(8, rectangle.area());
+    }
 }
